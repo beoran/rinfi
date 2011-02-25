fce4b96ec926cf032a4e74815b87334769bf3836
Beoran <beoran@rubyforge.org>
2011-02-25 07:52:16 +0100

    Adding empty files to define structure.

---
+++ lib/rinfi.rb
@@ -0,0 +1,8 @@
+module Rinfi
+  autoload :Main      , 'rinfi/main'
+  autoload :Parser    , 'rinfi/parser'
+  autoload :World     , 'rinfi/world'
+  autoload :Location  , 'rinfi/location'
+  autoload :Action    , 'rinfo/action'
+  autoload :Thing     , 'rinfo/thing'
+end
