5ef98a013180165fca5f055d76542a620498bd44
Beoran <beoran@rubyforge.org>
2011-05-16 15:54:21 +0200

    Parser.

--- lib/rinfi/parser.rb
+++ lib/rinfi/parser.rb
@@ -1,4 +1,4 @@
-require 'parselet'
+require 'parslet'
 =begin 
   A door is a thing.
   A color is a quality.
@@ -17,70 +17,24 @@ require 'parselet'
 
 
 module Rinfi
-  class Parser < Parselet::Parser
+  class Parser < Parslet::Parser
     
-    def initialize(world)
-      super()
-      @world = world
-    end
+     def initialize(world=nil)
+       super()
+       @world = world
+     end
     
     def stri(str)
       key_chars = str.split(//)
-      key_chars.
-        collect! { |char| match["#{char.upcase}#{char.downcase}"] }.
-        reduce(:>>)
+      aid       = key_chars.collect! do |char| 
+        match["#{char.upcase}#{char.downcase}"] 
+      end
+      aid.reduce(:>>)
     end
+
+    root(:clause)
     
-    rule(:space)         { match[ \t].repeat }
-    rule(:space?)        { space? }
-    rule(:crlf)          { match['\r']  >> match['\n']                        }
-    rule(:cr)            { match['\r']                                        }
-    rule(:lf)            { match['\n']                                        }
-    rule(:cr_or_lf)      { lf  | crlf | cr                                    }
-    
-    
-    # Comments
-    rule(:comment) do
-      str('(') >> (str(')').absent? >> any).repeat.as(:comment) |
-      str('(') >> comment >>  (str(')')
-    end
-    
-      
-    rule :dqstring do
-      str('"') >>
-      (
-        (str('\\') >> any) |
-        (str('"').absent? >> any)
-      ).repeat.as(:string) >>
-      str('"')
-    end
-  
-    rule :sqstring do
-      str("'") >>
-      (
-        (str('\\') >> any) |
-        (str("'").absent? >> any)
-      ).repeat.as(:string) >>
-      str("'")
-    end
-    
-    rule(:string) do
-      qdstring || sqstringS
-    end
-    
-    
-    rule(:sentence_end)  { (str('.')| lf | crlf | cr) >> ws?                }
-    rule(:paragraph_end) { cr_or_lf.repeat(2) >> ws?                        }
-    
-    rule (:paragraph) do
-      ((sentence | comment | string).repeat.maybe).as(:paragraph) >> paragraph_end
-    end
-    
-    rule(:text) do
-      paragraph.repeat.maybe.as(:text)
-    end
-    
-    rule(:blanknldot)    { match[' .\t\r\n']                              }
+    rule(:blanknldot)    { match[' \.\t\r\n']                              }
     
     # this is subtle: use this to state that a rule must be followed
     # only by anything that rule can parse, however, the tokens parsed by 
@@ -88,7 +42,10 @@ module Rinfi
     def followed_by(rule)
       (rule.absent? >> any).absent?
     end
-  
+    
+    KEYWORDS = %w{if then else when in of while until is and or not to thing 
+                  being place it its with of}
+
     def self.keywords(*names)
       names.each do |name|
         rule("kw_#{name}") do 
@@ -96,19 +53,201 @@ module Rinfi
         # so we say, not followe by(not blank followed by any character)
         # which means "followed by a blank character"
           stri(name.to_s).as(name) >> followed_by(blanknldot)
-        end  
+        end
       end
     end
 
-    keywords :if, :then, :else, :when, :in, :of, :while, :until, :is, :object
-
-    def self.ignore_words(*names)
-      rule("ignore_words") do 
-        names.each do |name|
-          str(name.to_s).as(name) >> followed_by(blanknldot)
+    keywords *KEYWORDS
+    
+    rule(:ws1)           { match[' \t']  | cr_or_lf >> cr_or_lf.absent?()     }
+    rule(:ws)            { ws1.repeat(1)                                      }
+    rule(:ws?)           { ws1.repeat(0)                                      }
+    rule(:crlf)          { match["\r"]  >> match["\n"]                        }
+    rule(:cr)            { match["\r"]                                        }
+    rule(:lf)            { match["\n"]                                        }
+    rule(:cr_or_lf)      { lf  | crlf | cr                                    }
+    
+    IGNORED_WORDS = %w{a an the} 
+    
+    rule :ignore do 
+      res = nil
+      IGNORED_WORDS.each do |name|
+        aid =  (stri(name.to_s) >> followed_by(blanknldot))
+        if !res 
+          res = aid
+        else
+          res = res | aid  
         end  
       end
+      res # will be ignored because there is no .as rule
+    end
+    
+    rule :keyword do
+      res = nil
+      KEYWORDS.each do |name|
+        rule  = self.method("kw_#{name}".to_sym).call()
+        aid   = rule
+        # aid = (stri(name.to_s) >> followed_by(blanknldot))
+        if !res 
+          res = aid
+        else
+          res = res | aid  
+        end  
+      end
+      res # will automatically return a keyword .as(:keyword)
+    end
+    
+    rule :apo_s do
+      str("'s").as(:apo_s)
+    end
+    
+    rule :number do
+      match([0-9]).repeat(1)
+    end
+    
+    rule :plain_word do
+      (match["A-Za-z"].repeat(1)).as(:word)
+    end
+    
+    rule :apo_s_word do
+      (plain_word >> ws? >> apo_s).as(:apo_s_word)
+    end
+    
+    
+    rule :word do
+       number | keyword | ignore | apo_s_word | plain_word 
+    end
+    
+    rule :word_nokw do
+      keyword.absent? >> (number | ignore | apo_s_word | plain_word)
+    end
+    
+    rule :no_kw_clause do
+      (word_nokw >> (ws >> word_nokw).repeat(0)).as(:nokw_clause)
     end
 
+    rule :builtin do
+      ( kw_thing | kw_place | kw_being )
+    end
+    
+    rule :word_nokwbi do
+      word_nokw | builtin
+    end
+    
+    rule :no_kwbi_clause do
+      (word_nokwbi >> (ws >> word_nokwbi).repeat(0)).as(:nokwbi_clause)
+    end
+
+    rule :is_clause do
+      (no_kw_clause >> ws >> kw_is >> ws >> no_kwbi_clause).as(:is_clause) 
+    end
+    
+    rule :generic_clause do
+      (word >> (ws >> word).repeat(0)).as(:clause)
+    end
+
+    rule :clause do
+      is_clause         |
+      generic_clause 
+    end
+
+    rule(:clause_end)  do
+       ws? >> match['\;\:\,'] >> ws?
+    end
+    
+    rule(:question_end) do 
+      str('?').as(:sign) >> ws?
+    end
+
+    rule(:exclaim_end) do 
+      str('!').as(:sign) >> ws?
+    end
+    
+    rule(:sentence_end) do 
+      str('.').as(:sign) >> ws?
+    end
+    
+    rule(:clause_list) do
+      (clause >> (clause_end >> clause).repeat(0))
+    end
+    
+
+    rule :exclaim_sentence do
+      clause_list.as(:exclaim)   >>  exclaim_end
+    end
+
+    rule :question_sentence do
+      clause_list.as(:question)  >>  question_end
+    end
+   
+    rule :normal_sentence do
+      clause_list.as(:sentence)  >>  sentence_end
+    end
+   
+    # Comments
+    rule(:comment) do
+      str('(') >> (str(')').absent? >> any).repeat.as(:comment) >> str(')') >> ws? 
+    end
+    
+      
+    rule :dqstring do
+      str("\"") >>
+      (
+        (str('\\') >> any) |
+        (str("\"").absent? >> any)
+      ).repeat.as(:string) >>
+      str("\"")
+    end
+  
+    rule :sqstring do
+      str("\'") >>
+      (
+        (str('\\') >> any) |
+        (str("\'").absent? >> any)
+      ).repeat.as(:string) >>
+      str("\'")
+    end
+    
+    rule(:string) do
+      (dqstring | sqstring) >> ws?
+    end
+
+    
+    rule :sentence do
+      normal_sentence | question_sentence | exclaim_sentence | comment | string
+    end
+    
+    rule :paragraph do
+       (sentence >> (sentence).repeat(0)).as(:paragraph)
+    end
+    
+    rule(:paragraph_end) { cr_or_lf.repeat(2) >> ws?                        }
+    
+    rule(:text) do
+      (paragraph >> (paragraph_end >> paragraph).repeat(0)).as(:text)
+    end
+
+
+=begin
+
+
+    
+    
+    
+    
+    
+
+    
+    
+    
+    rule (:paragraph) do
+      ((sentence | comment | string).repeat.maybe).as(:paragraph)
+    end
+    
+    
+    
+
+    
+=end
   end
 end
--- test/rinfi/test_parser.rb
+++ test/rinfi/test_parser.rb
@@ -1,40 +1,137 @@
 require 'test_helper'
 require 'rinfi'
+require 'parslet'
+require 'parslet/convenience'
+
+
+def parse(rule, text)
+  parser = Rinfi::Parser.new
+  rule   = parser.send(rule.to_sym)
+  return rule.parse_with_debug(text)
+  rescue $! 
+  p $!
+  puts $!.backtrace.join("\n")
+  return nil 
+end
+
+def noparse(rule, text)
+  parser = Rinfi::Parser.new
+  rule   = parser.send(rule.to_sym)
+  rule.parse(text)
+  return false
+  rescue $! 
+  # p $!
+  return true 
+end
+
 
 assert { Rinfi::Parser }
 include Rinfi
-assert { Parser } 
-assert { Parser.normalize("Hello\rworld") == "Hello\nworld" } 
-assert { Parser.normalize("Hello\\\rworld") == "Hello world" }
-assert { Parser.normalize("Hello,  \nworld") == "Hello, world" }
+assert { Parser }
+res = nil 
+ 
 
-parser = nil 
-assert { parser = Parser.new(nil) } 
-para = nil 
-txt1 = %{Para 1 Line 1
-Para 1 Line 2
- 
-Para 2 Line 1.      Para 2 Line 2
-Para 2 Line 3
- 
-Para 3 Line 1 
-} 
-assert { para = parser.split_paragraphs(txt1) }
-assert { para.size == 3 } 
-assert { para.last == "Para 3 Line 1 \n" }
-lines = nil 
-assert { lines = parser.split_lines(para[1]) }
-assert { lines.size == 3 } 
-assert { lines.last == "Para 2 Line 3" } 
- 
-tst2 = "This line has several reading signs: the colon, the semicolon and the comma; but it should parse correctly and ignore extrateranous  spaces   like  these "
+assert { parse :ws                  , "    "          }
+assert { parse :ws                  , " \n\t"         }
+assert { parse :ws                  , " 
+       "  }
+assert { noparse :ws                , "Hello"         }
+assert { noparse :ws                , "n"             }
+assert { noparse :ws                , "t"             }
+assert { parse(:word                , "hello")        }
+assert { noparse :word              , "   "           }
+assert { noparse(:word              , "hello world")  }
+assert { parse(:word_nokw           , "hello")        }
+assert { noparse(:word_nokw         , "is")           }
+assert { parse :no_kw_clause        , "open the gate" }
+assert { noparse :no_kw_clause      , "door is thing" }
+assert { res = parse :clause        , "hello world"   }
+assert { res = parse :sentence_end  , ". "            }
+assert { res = parse :exclaim_end   , "! "            }
+assert { res = parse :question_end  , "? "            }
+assert { res = noparse :sentence_end, " "             }
+assert { tes = parse :is_clause     , "A door is a thing"        }
+assert { res = parse :sentence      , "Hello world."             }
+assert { res = parse :sentence      , "The door's size is ten."  }
+assert { res = parse :sentence      , "The door's size is ten."  }
+assert { res = parse :sentence      , "Hello world, this is me." }
+assert { res = parse :paragraph     , %{Hello world, this is me!
+Life should be, yeah, fun for everyone.   }
+}
 
-words = nil
-assert { words = parser.split_words(tst2) }
-p words
-assert { words.size == 27 }
-res = nil   
-assert { res = parser.parse(tst2) }
+assert { res = parse :text          , %{Hello world, this is me!
+Life should be, yeah, fun for everyone.
+
+And it goes on here in another paragraph.
+}
+}
+
+assert do res = parse :text          , %{I say hello to the world, this is me!
+Life should be, yeah, fun for everyone. "And a String 
+too"
+
+And it goes on here in another paragraph.
+(And there is comment)
+
+The door's size is 10 .
+}
+end
+
+
+
+=begin
+assert { parse :sentence            , %{hello.}         }
+assert { res = parse :sentence      , %{hello , world.} }
+assert { parse(:comment             , "(hello comment)") }
+assert { parse :sqstring            , %{'hello'}      }
+assert { parse :dqstring            , %{"hello"}      }
+assert { parse :string              , %{'hello'}      }
+assert { parse :string              , %{"hello"}      }
+=end
+
+
+# assert { res = parse(:generic_clause, "hello world") }
+p res
+
+# assert { parse(:clause        , "hello world") }
+
+
+
+
+
+
+# 
+# assert { Parser.normalize("Hello\rworld") == "Hello\nworld" } 
+# assert { Parser.normalize("Hello\\\rworld") == "Hello world" }
+# assert { Parser.normalize("Hello,  \nworld") == "Hello, world" }
+# 
+# parser = nil 
+# assert { parser = Parser.new(nil) } 
+# para = nil 
+# txt1 = %{Para 1 Line 1
+# Para 1 Line 2
+#  
+# Para 2 Line 1.      Para 2 Line 2
+# Para 2 Line 3
+#  
+# Para 3 Line 1 
+# } 
+# assert { para = parser.split_paragraphs(txt1) }
+# assert { para.size == 3 } 
+# assert { para.last == "Para 3 Line 1 \n" }
+# lines = nil 
+# assert { lines = parser.split_lines(para[1]) }
+# assert { lines.size == 3 } 
+# assert { lines.last == "Para 2 Line 3" } 
+#  
+# tst2 = "This line has several reading signs: the colon, the semicolon and the comma; but it should parse correctly and ignore extrateranous  spaces   like  these "
+# 
+# words = nil
+# assert { words = parser.split_words(tst2) }
+# p words
+# assert { words.size == 27 }
+# res = nil   
+# assert { res = parser.parse(tst2) }
 
 
 
